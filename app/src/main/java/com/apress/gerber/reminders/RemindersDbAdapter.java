6db4f3b352bf34fe2525956525c7ae434c2c2e72
root <trejo85@live.com.mx>
2015-12-28 13:35:43 -0600

    Actualizacion

--- app/src/main/java/com/apress/gerber/reminders/RemindersDbAdapter.java
+++ app/src/main/java/com/apress/gerber/reminders/RemindersDbAdapter.java
@@ -1,5 +1,10 @@
 package com.apress.gerber.reminders;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
 /**
  * Created by root on 28/12/15.
  */
@@ -9,4 +14,48 @@ public class RemindersDbAdapter {
     public static final String COL_CONTENT="content";
     public static final String COL_IMPORTANT="important";
 //    these are the corresponding indices
+    public static final int INDEX_ID = 0;
+    public static final int INDEX_CONTENT = INDEX_ID + 1;
+    public static final int INDEX_IMPORTANT = INDEX_ID + 2;
+//    used for loggin
+    private static final String TAG ="RemindersDbAdapter";
+
+    private DatabaseHelper mDbHerlper;
+    private SQLiteDatabase mDB;
+
+    private static final String DATABASE_NAME = "dba_remdrs";
+    private static final String TABLE_NAME = "tbl_remdrs";
+    public static final int DATABASE_VERSION = 1;
+
+    private final Context mCtx = null;
+
+//    SQL statement used to create the database
+
+    private static final String  DATABASE_CREATE=
+            "CREATE TABLE if not exists" + TABLE_NAME +  "(" +
+                    COL_ID + " INTEGER PRIMARY KEY autoincrement, " +
+                    COL_CONTENT + "TEXT," + COL_IMPORTANT + " INTEGER );";
+
+    private static class DatabaseHelper extends SQLiteOpenHelper {
+        DatabaseHelper(Context context){
+            super(context, DATABASE_NAME,null,DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            Log.w(TAG,DATABASE_CREATE);
+            db.execSQL(DATABASE_CREATE);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            Log.w(TAG,"Upgrading database from version" + oldVersion + "to"
+                    + newVersion + ",which will destroy all old data");
+            db.execSQL("DROP TABLE IF EXISTS"+TABLE_NAME);
+            onCreate(db);
+        }
+    }
+
+
+
 }
